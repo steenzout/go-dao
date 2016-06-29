1ad3dfb1c43f8e6ed82c5b31e0c6a387d34a1f4f
Pedro Salgado <steenzout@ymail.com>
2016-06-29 14:10:24 -0600

    rename test function

--- package_test.go
+++ package_test.go
@@ -1,12 +1,13 @@
 package dao_test
 
 import (
-	"github.com/stretchr/testify/suite"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
-// PackageTestSuite test suite for the dao package.
-func PackageTestSuite(t *testing.T) {
+// TestPackage test suite for the dao package.
+func TestPackage(t *testing.T) {
 	suite.Run(t, new(DAOTestSuite))
 	suite.Run(t, new(HandlerTestSuite))
 	suite.Run(t, new(ManagerTestSuite))
